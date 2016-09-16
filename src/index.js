49e846ac38d5104ffed5444b55429bf73983f5d2
殊十二 <ljm_90@163.com>
2016-09-16 18:23:39 +0800

    修改入口路径

---
+++ src/index.js
@@ -0,0 +1,27 @@
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends React.Component {
+	construtor(props) {
+		super(props);
+		this.state = {
+
+		};
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Hello,World!</h1>
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(
+	<App />,
+	document.getElementById('app')
+);*/
+
+var a = 1;
+console.log(a);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+/*const HTMLWebpackPluginConfig = new HtmlWebpackPlugin({
+	template: `${__dirname}/src/index.html`,
+	filename: 'index.html',
+	inject: 'body',
+});*/
+
+module.exports = {
+	entry: [
+		'src/index.js'
+	],
+
+	output: {
+		path: `${__dirname}/build`,
+		filename: 'index_bundle.js',
+	},
+
+	module: {
+		loaders:[
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			}
+		]
+	}
+
+	/*devServer: {
+		inline: true,
+		port: 8008,
+	},*/
+
+	//plugins: [HTMLWebpackPluginConfig]
+};
